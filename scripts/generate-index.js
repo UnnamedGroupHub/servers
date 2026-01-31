33cc7a7b38ad77b4fb7441e2f538788b46153d76
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-01-31 15:45:43 +0000

    more styles :£

--- scripts/generate-index.js
+++ scripts/generate-index.js
@@ -97,9 +97,10 @@ const html = `<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Servers Index</title>
   <style>
-    @import url("https://use.typekit.net/oqu5sxm.css");
     html {
-      font-family: "purista-web", monospace;
+      background: black;
+      color: white;
+      font-family: monospace;
     }
     summary {
       cursor: pointer;
